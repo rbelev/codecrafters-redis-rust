9861e52d021db0b35d6a7ffd3363aae50353b41c
Ryan Belev <ryan.belev@gmail.com>
2025-07-16 12:14:43 -0700

    refactor to passing MutexGuard(?); returning Value and serializing in process

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use std::env;
 use std::error::Error;
 use std::ops::Add;
 use std::str;
-use std::sync::{Arc, Mutex};
+use std::sync::{Arc, Mutex, MutexGuard};
 use std::time::{Duration, SystemTime};
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpListener;
@@ -33,7 +33,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
                     Ok(0) => return,
                     Ok(n) => n,
                     Err(e) => {
-                        eprintln!("failed to read from socket; err = {:?}", e);
+                        eprintln!("failed to read from socket; err = {e:?}");
                         return;
                     }
                 };
@@ -41,7 +41,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
                 let response = process(command, &redis);
                 if let Err(e) = socket.write_all(&response.into_bytes()).await {
-                    eprintln!("failed to write to socket; err = {:?}", e);
+                    eprintln!("failed to write to socket; err = {e:?}");
                     return;
                 };
             }
@@ -56,14 +56,14 @@ fn process(buff: &str, store: &Redis) -> String {
     let mut lines = buff.split("\r\n");
     let parsed_value = Value::parse(&mut lines);
 
-    eval_command(&parsed_value, store)
+    let store = store.lock().unwrap();
+    let response = eval_command(&parsed_value, store);
+    response.serialize()
 }
 
-fn eval_command(segments: &Value, store: &Redis) -> String {
+fn eval_command(segments: &Value, store: MutexGuard<DB>) -> Value {
     match segments {
         Value::Array(arr) => {
-            println!("eval_command array-array-array: {:?}", arr);
-
             match &arr[0] {
                 Value::BulkString(cmd) if cmd == "ECHO" => eval_echo(&arr[1..]),
                 Value::BulkString(cmd) if cmd == "PING" => eval_ping(),
@@ -71,7 +71,8 @@ fn eval_command(segments: &Value, store: &Redis) -> String {
                 Value::BulkString(cmd) if cmd == "GET" => eval_get(&arr[1..], store),
                 Value::BulkString(cmd) if cmd == "KEYS" => eval_keys(&arr[1..], store),
                 Value::BulkString(cmd) if cmd == "CONFIG" => eval_config(&arr[1..], store),
-                Value::BulkString(cmd) => panic!("Not a valid command: {}", cmd),
+                // Value::BulkString(cmd) if cmd == "INCR" => eval_incr(&arr[1..], store),
+                Value::BulkString(cmd) => panic!("Not a valid command: {cmd}"),
                 _ => panic!("non-BulkString first: {}", &arr[0].serialize()),
             }
         }
@@ -79,9 +80,8 @@ fn eval_command(segments: &Value, store: &Redis) -> String {
     }
 }
 
-fn eval_set(params: &[Value], store: &Redis) -> String {
-    println!("set params: {:?}", params);
-    let mut store = store.lock().unwrap();
+fn eval_set(params: &[Value], mut store: MutexGuard<DB>) -> Value {
+    println!("set params: {params:?}");
 
     match params {
         [Value::BulkString(name), Value::BulkString(value)] => {
@@ -107,69 +107,41 @@ fn eval_set(params: &[Value], store: &Redis) -> String {
         _ => {}
     };
 
-    Value::SimpleString(String::from("OK")).serialize()
+    Value::SimpleString(String::from("OK"))
 }
-fn eval_get(params: &[Value], store: &Redis) -> String {
-    println!("get params: {:?}", params);
-    let store = store.lock().unwrap();
-
-    let value: Option<&db::StoredValue> = match params {
-        [Value::BulkString(name)] => store.db.get(name),
+fn eval_get(params: &[Value], store: MutexGuard<DB>) -> Value {
+    let value: Option<&db::StoredValue> = match params.first() {
+        Some(Value::BulkString(name)) => store.db.get(name),
         _ => None,
     };
-    match value {
-        Some(opt) => match opt.expiry {
-            Some(expires_at) => {
-                if SystemTime::now() >= expires_at {
-                    return String::from(Value::NULL_STRING);
-                }
-                println!("Not expired: {expires_at:?} > {:?}", SystemTime::now());
-                println!(
-                    "Time till Expiry: {:?}",
-                    expires_at.duration_since(SystemTime::now())
-                );
 
-                opt.value.serialize()
-            }
-            None => {
-                println!("No expiry set");
-                opt.value.serialize()
-            }
-        },
-        None => String::from(Value::NULL_STRING),
+    match value {
+        Some(opt) if !opt.is_expired() => opt.value.clone(),
+        _ => Value::SimpleString(Value::NULL_STRING.to_string()),
     }
 }
 
-fn eval_echo(params: &[Value]) -> String {
-    params[0].serialize()
+fn eval_echo(params: &[Value]) -> Value {
+    params[0].clone()
 }
 
-fn eval_ping() -> String {
-    Value::SimpleString(String::from("PONG")).serialize()
+fn eval_ping() -> Value {
+    Value::SimpleString(String::from("PONG"))
 }
 
-fn eval_config(params: &[Value], store: &Redis) -> String {
-    println!("eval_config: {:?}", params);
-    let store = store.lock().unwrap();
-
+fn eval_config(params: &[Value], store: MutexGuard<DB>) -> Value {
     // Assumed GET, so skipping past [0].
-    let config_value: &str = match &params[1] {
+    let field = params[1].clone();
+    let config_value = match &field {
         Value::BulkString(tar) if tar == "dir" => &store.config.dir,
         Value::BulkString(tar) if tar == "dbfilename" => &store.config.dbfilename,
         bad_tar => panic!("unknown config: {}", bad_tar.serialize()),
     };
-    println!("config_value: {:?}", config_value);
 
-    Value::Array(vec![
-        params[1].clone(),
-        Value::BulkString(String::from(config_value)),
-    ])
-    .serialize()
+    Value::Array(vec![field, Value::BulkString(config_value.clone())])
 }
 
-fn eval_keys(params: &[Value], store: &Redis) -> String {
-    let store = store.lock().unwrap();
-
+fn eval_keys(params: &[Value], store: MutexGuard<DB>) -> Value {
     match &params[0] {
         Value::BulkString(all) if all == "*" => {
             let all = store
@@ -177,8 +149,26 @@ fn eval_keys(params: &[Value], store: &Redis) -> String {
                 .keys()
                 .map(|key| Value::BulkString(key.clone()))
                 .collect::<Vec<Value>>();
-            Value::Array(all).serialize()
+            Value::Array(all)
         }
-        _ => panic!("eval_keys: only * is supported: {:?}", params),
+        _ => panic!("eval_keys: only * is supported: {params:?}"),
     }
 }
+
+// fn eval_incr(params: &[Value], store: &Redis) -> String {
+//     let store = store.lock().unwrap();
+//     let key = params.get(0);
+//
+//     let value: Option<&db::StoredValue> = match key {
+//         Some(Value::BulkString(name)) => store.db.get(name),
+//         _ => None,
+//     };
+//     match value {
+//         None => {
+//             store.db.insert(params.get(0), 1);
+//         }
+//         Some(Value::Integer(integer)) => {
+//             store.db.insert(params.get(0), integer + 1);
+//         }
+//     }
+// }
