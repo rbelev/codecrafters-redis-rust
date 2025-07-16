a186c9ac19105043dca7694cf17c67f3a880de27
Ryan Belev <ryan.belev@gmail.com>
2025-07-16 11:21:45 -0700

    improve Config impl

--- src/config.rs
+++ src/config.rs
@@ -1,23 +1,29 @@
 use std::path::{Path, PathBuf};
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct Config {
     pub dir: String,
     pub dbfilename: String,
 }
 
 impl Config {
+    pub const DEFAULT_DATA_DIR: &'static str = "/tmp/redis-data";
+    pub const DEFAULT_DATA_FILE: &'static str = "rdbfile.rdb";
+
     pub fn new(mut args: Vec<String>) -> Config {
-        // println!("{args:?}");
         let dir: String = match args.iter().position(|arg| arg == "--dir") {
             Some(pos) => args.remove(pos + 1),
-            None => String::from("/tmp/redis-data"),
+            None => String::from(Self::DEFAULT_DATA_DIR),
         };
         let dbfilename: String = match args.iter().position(|arg| arg == "--dbfilename") {
             Some(pos) => args.remove(pos + 1),
-            None => String::from("rdbfile.rdb"),
+            None => String::from(Self::DEFAULT_DATA_FILE),
         };
-        Config { dir, dbfilename }
+        Config {
+            dir,
+            dbfilename,
+            // ..Self::default()
+        }
     }
 
     pub fn rbd(&self) -> PathBuf {
@@ -25,30 +31,43 @@ impl Config {
     }
 }
 
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            dir: String::from(Self::DEFAULT_DATA_DIR),
+            dbfilename: String::from(Self::DEFAULT_DATA_FILE),
+        }
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
 
-    impl PartialEq for Config {
-        fn eq(&self, other: &Self) -> bool {
-            self.dir == other.dir && self.dbfilename == other.dbfilename
-        }
-    }
     #[test]
-    fn test_args() {
+    fn test_custom_args() {
+        let my_dir = String::from("/tmp/my-data");
+        let my_db_file = String::from("my_redis.rdb");
         let args = vec![
             String::from("--dir"),
-            String::from("/tmp/my-data"),
+            my_dir.clone(),
             String::from("--dbfilename"),
-            String::from("my_redis.rdb"),
+            my_db_file.clone(),
         ];
+
         let config = Config::new(args);
         assert_eq!(
             config,
             Config {
-                dir: String::from("/tmp/my-data"),
-                dbfilename: String::from("my_redis.rdb"),
+                dir: my_dir,
+                dbfilename: my_db_file,
             }
         );
+    }
+
+    #[test]
+    fn test_default_args() {
+        let config = Config::new(Vec::new());
+        assert_eq!(config, Config::default());
     }
 }
